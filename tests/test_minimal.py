2295bb30cefd398df74e59cad40551136a593e51
Christopher <105308934+helicopterrun@users.noreply.github.com>
2025-11-25 10:52:27 -0800

    Delete tests/test_minimal.py

--- tests/test_minimal.py
+++
@@ -1,8 +0,0 @@
-"""Minimal test to verify setup works."""
-from custom_components.phyn.const import DOMAIN
-
-
-def test_domain_exists():
-    """Test that the domain constant exists."""
-    assert DOMAIN == "phyn"
-    assert len(DOMAIN) > 0
